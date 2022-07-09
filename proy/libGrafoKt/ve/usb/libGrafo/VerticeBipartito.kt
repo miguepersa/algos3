7434a21ea31bbb12898fe94827a07dda328725bb
danielrobapaz <87443087+danielrobapaz@users.noreply.github.com>
2022-07-09 19:42:47 -0400

    listo proy act1 y act2

--- proy/libGrafoKt/ve/usb/libGrafo/VerticeBipartito.kt
+++
@@ -1,9 +0,0 @@
-package ve.usb.libGrafo
-
-/* 
-    Tipo de vertice usado en la clase GrafoBipartito
-*/
-data class VerticeBipartito(val n: Int) {
-    var color: Color = Color.BLANCO
-    var kcolor: Int = 1
-}
