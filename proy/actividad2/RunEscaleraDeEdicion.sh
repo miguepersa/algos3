4d3e748d9b917767348f59e43d06e0235225da77
danielrobapaz <87443087+danielrobapaz@users.noreply.github.com>
2022-07-17 18:33:33 -0400

    aestrella no compila

--- proy/actividad2/RunEscaleraDeEdicion.sh
+++ proy/actividad2/RunEscaleraDeEdicion.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+export JAVA_OPTS="-Xmx2g"
 kotlin -cp libGrafoKt/libGrafoKt.jar:. EscaleraDeEdicionKt.class  $*
--- sem8/libGrafoKt/ve/usb/libGrafo/AEstrella.kt
+++ sem8/libGrafoKt/ve/usb/libGrafo/AEstrella.kt
@@ -1,5 +1,11 @@
 package ve.usb.libGrafo
 
+data class VerticeAEstrella(val n: Int): Vertice {
+   var pred: VerticeAEstrella? = null
+   var d: Double = Double.MAX_VALUE
+   var fHat: Double = 0.0
+}
+
 /*
  Implementación del algoritmo de A* para encontrar un camino
  desde un vértice fuente s fijo hasta alguno de los vértices objetivos. 
@@ -20,16 +26,87 @@ package ve.usb.libGrafo
     * Al menos uno de los vértices de objs debe ser alcanzable desde s. Esta precondición no se chequea por razones de eficiencia.
 */
 public class AEstrella(val g: GrafoDirigidoCosto, 
-		       val s: Int,
-		       val objs: Set<Int>,
-		       val hHat: (Int) -> Double) {
-    
-    // Retorna el vértice meta del conjunto objs que fue alcanzado por el algoritmo.
-    fun objetivoAlcanzado() : Int { }
+		      val s: Int,
+		      val objs: Set<Int>,
+		      val hHat: (Int) -> Double) {
+   
+   var listaVertices: MutableList<VerticeAEstrella>
+   var verticeAlcanzado: Int
 
-    // Retorna el costo del camino desde s hasta el vértice objetivo alcanzado. 
-    fun costo() : Double {}
+   init {
+      // Verificamos que el grafo no tenga lado con costo negativo
+      for (l in g) {
+         if (l.obtenerCosto() < 0) { 
+            throw RuntimeException("AEstrella.init: El grafo dado tiene lado con costo negativo")
+         }
+      }
 
-    // Retorna los arcos del camino desde s hasta el vértice objetivo alcanzado. 
-    fun obtenerCamino() : Iterable<ArcoCosto> { }
+      // Verificamos que el vertice s pertenezca al grafo
+      if (s < 0 || s >= g.obtenerNumeroDeVertices()) {
+         throw RuntimeException("AEstrella.init: El vertice fuente $s no pertenece al grafo")
+      }
+
+      // Verificamos que los vertices objs pertenezcan al grafo
+      for (v in objs) {
+         if (v < 0 || v >= g.obtenerNumeroDeVertices()) {
+            throw RuntimeException("AEstrella.init: El vertive objetivo $v no pertenece al grafo")
+         }
+      }
+
+      var closed: MutableList<Int> = mutableListOf()
+      var inClosed: MutableList<Boolean> = mutableListOf() // para determinar si esta o no un elemento en closed
+      
+      // Inicializamos los arreglos inClosed y inOpenQueue
+      for (i in 0 until g.obtenerNumeroDeVertices()) inClosed.add(false) // no hay ningun elemento en closed
+            
+      var openQueue: ColaDePrioridad = ColaDePrioridad()
+      var inOpenQueue: MutableList<Boolean> = mutableListOf() // para determinar si esta o no un elemento en openQueue
+
+      for (i in 0 until g.obtenerNumeroDeVertices()) inOpenQueue(false)
+      inOpenQueue[s].true
+
+      listaVertices = mutableListOf()
+      for (i in 0 until g.obtenerNumeroDeVertices()) {
+         listaVertices.add(VerticeAEstrella(i))
+      }
+
+      listaVertices[s].fHat = hHat
+      listaVertices[s].pred = null
+      openQueue.add(listaVertices[s])
+
+      var u: Int = openQueue.extraerMinimo()    
+
+      while (!objs.contains(u)) {
+         closed.add(u)
+
+         for (v in g.adyacentes(u)) {
+            if (!inClosed(v.y.n)) {
+               val fHatNew = listaVertices[u].fHat + hHat(u) + v.obtenerCosto() + hHat(v.y.n)
+
+               if (!inOpenQueue(v.y.n)) { // Acomodar lista de boolean
+                  listaVertices[v.y.n].fHat = fHatNew
+                  listaVertices[v.y.n].pred = listaVertices[u]
+                  openQueue.add(listaVertices[v.y.n])
+               } else {
+                  if (fHatNew < listaVertices[v.y.n].fHat) {
+                     listaVertices[v.y.n].fHat = fHatNew
+                     listaVertices[v.y.n].pred = listaVertices[u]
+                  }
+               }
+            }
+         }
+
+         u = openQueue.extraerMinimo()
+      }
+
+      verticeAlcanzado = u      
+   }
+   // Retorna el vértice meta del conjunto objs que fue alcanzado por el algoritmo.
+   // fun objetivoAlcanzado() : Int { }
+
+   // Retorna el costo del camino desde s hasta el vértice objetivo alcanzado. 
+   // fun costo() : Double {}
+
+   // Retorna los arcos del camino desde s hasta el vértice objetivo alcanzado. 
+   // fun obtenerCamino() : Iterable<ArcoCosto> { }
 }
