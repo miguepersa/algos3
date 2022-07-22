2719bb93cdde2422be95aee3dd5b4cc77553c423
miguepersa <miguepersa@gmail.com>
2022-07-22 18:03:15 -0400

    Johnson refresco

--- sem9/Main.kt
+++ sem9/Main.kt
@@ -2,30 +2,29 @@ import ve.usb.libGrafo.*
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-fun h(i: Int): Double {
-    return 0.0
-}
-
 fun main(args: Array<String>) {
     val grafo = GrafoDirigidoCosto(args[0])
-    val s: Int = 4
+    var j = Johnson(grafo)
+    var d = j.obtenerMatrizDistancia()
+    var p = j.obtenerMatrizPredecesores()
 
-/*     var bfs = BFS(grafo, 1)
-    for (i in 0 until grafo.nVertices) {
-        if (grafo.arrVertices[i].d != Int.MAX_VALUE) {
-            println(bfs.caminoHasta(i))
+    println("\n\n Distancias:")
+    for (i in d) {
+        var linea = "[\t"
+        for (j in i) {
+            linea = linea + "\t" + j.toString()
         }
-    } */
+        linea = linea + "\t]"
+        println(linea)
+    }
 
-    /* for (i in 0 until 5) {
-        if (i != s) {
-    
+    println("\n\n Predecesores:")
+    for (i in p) {
+        var linea = "[\t"
+        for (j in i) {
+            linea = linea + "\t" + j.toString()
         }
-        
-    } */
-    var prueba = AEstrella(grafo, 1, setOf(4), {0.0})
-    println(prueba.objetivoAlcanzado())
-    println(prueba.costo())
-    println(prueba.obtenerCamino())
-    println()
+        linea = linea + "\t]"
+        println(linea)
+    }
 }
