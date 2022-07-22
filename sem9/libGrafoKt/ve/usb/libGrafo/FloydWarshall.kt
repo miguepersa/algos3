4efb7cbf162617a4ce6445ba07601b3c4e66f8c6
danielrobapaz <87443087+danielrobapaz@users.noreply.github.com>
2022-07-22 18:12:18 -0400

    listo floyd

--- sem9/libGrafoKt/ve/usb/libGrafo/FloydWarshall.kt
+++ sem9/libGrafoKt/ve/usb/libGrafo/FloydWarshall.kt
@@ -11,27 +11,154 @@ package ve.usb.libGrafo
  La matriz de entrada es cuadrada de dimensiones nxn, donde n es el número de 
  vértices del grafo. Si la matriz de entrada no es cuadrada, entoces se lanza una RuntimeException.
  */
-public FloydWarshall(val W : Array<Array<Double>>) {
+public class FloydWarshall(val W : Array<Array<Double>>) {
+    var matrizD: Array<Array<Double>>
+    var matrizPred: Array<Array<Int?>>
+    var nVertices: Int
+    init {
+        nVertices = W.size
 
-    // Retorna la matriz con las distancias de los caminos de costo mínimo
-    // entre todos los pares de vértices.
-    fun obtenerMatrizDistancia() : Array<Array<Double>> { } 
+        for (i in 0 until nVertices) {
+            if (W[i].size != nVertices) throw RuntimeException("FloydWarshall.init: La matriz dada no es cuadrada")
+        }
 
-    // Retorna la matriz con los predecesores de todos los vértices en los caminos de costo mínimo
-    // entre todos los pares de vértices.
-    fun obtenerMatrizPredecesores() : Array<Array<Int?>> { } 
+        matrizD = W // Copiamos la matriz W
+
+        // Llenamos la matriz de predecesores
+        matrizPred = Array<Array<Int?>>(nVertices, {i -> Array<Int?>(nVertices, {j -> null})})
+        for (i in 0 until nVertices) {
+            for (j in 0 until nVertices) {
+                if (i == j || matrizD[i][j] == Double.POSITIVE_INFINITY) {
+                    matrizPred[i][j] = null
+                } else if (i != j || matrizD[i][j] < Double.POSITIVE_INFINITY) {
+                    matrizPred[i][j] = i
+                }
+            }
+        }
+
+        for (k in 0 until nVertices) {
+            for (i in 0 until nVertices) {
+                for (j in 0 until nVertices) {
+                    if (matrizD[i][j] > matrizD[i][k] + matrizD[k][j]) { // Solo revisamos este caso, pues el otro mantiene las matrices iguales
+                        matrizD[i][j] = matrizD[i][k] + matrizD[k][j] // modificamos el costo del camino
+                        matrizPred[i][j] = matrizPred[k][j] // Modificamos el predecesor
+                    }
+                }
+            }
+        }
+    }
+
+    /* 
+        Devuelve la matriz las distancias del camino de costo minimo 
+        entre cada par de vertices
+
+        {P: true}
+        {Q: true}
+
+        Input: ~~
+        Output: Una matriz de reales que contiene las distancias
+
+        Tiempo de ejecucion O(1)
+    */
+    fun obtenerMatrizDistancia() : Array<Array<Double>> = matrizD
+
+    /* 
+        Devuelve la matriz de predecesores del camino de costo minimo
+        entre cada par de vertices
+
+        {P: true}
+        {Q: true}
+
+        Input: ~~
+        Output: Una matriz de Enteros o nulos que contiene los predecesores del camino de costo minimo
+
+        Tiempo de ejecucion O(1)
+    */
+    fun obtenerMatrizPredecesores() : Array<Array<Int?>> = matrizPred
     
-    // Retorna la distancia del camino de costo mínimo desde el vértice u hasta el vértice v.
-    // Si alguno de los dos vértices  no existe, se lanza una RuntimeException.
-    // Si no existe un camino desde u hasta v, entonces se lanza una RuntimeException.
-    fun costo(u: Int, v: Int) : Double { }
+    /* 
+        Devuelve el costo del camino de costo minio desde un vertice u hasta v
 
-    // Retorna cierto si hay un camino desde u hasta el vértice v.
-    // Si alguno de los dos vértices no existe, se retorna un RuntimeException.
-    fun existeUnCamino(u: Int, v: Int) : Boolean { }
+        {P: u y v son vertices del grafo y existe un camino desde u hasta v}
+        {Q: matrizD[u][v]}
 
-    // Retorna los arcos del camino de costo mínimo desde u hasta v.
-    // Si alguno de los dos vértices  no existe, se retorna un RuntimeException.
-    // Si no existe un camino desde u hasta v, entonces se lanza una RuntimeException.
-    fun obtenerCaminoDeCostoMinimo(u: Int, v: Int) : Iterable<Arco> { }
+        Input: u y v son vertices del grafo
+        Output: El costo del camino de costo minimo desde u hasta v
+
+        Tiempo de ejecucion O(1)
+    */
+    fun costo(u: Int, v: Int) : Double { 
+        if (u < 0 || u >= nVertices) {
+            throw RuntimeException("FloydWarshall.costo(): El vertice $u no pertenece al grafo")
+        }
+
+        if (v < 0 || v >= nVertices) {
+            throw RuntimeException("FloydWarshall.costo(): El vertice $v no pertenece al grafo")
+        }
+
+        if (!this.existeUnCamino(u, v)) {
+            throw RuntimeException("FloydWarshall.costo(): No hay camino desde $u hasta $v")
+        }
+
+        return matrizD[u][v]
+    }
+
+    /* 
+        Devuelve si es cierto que hay un cam ino desde u hasta v
+
+        {P: u y v son vertices del grafo}
+        {Q: matrizPred[u][v] != null}
+
+        Input: u y v vertices del grafo
+        Output: true -> Si existe un camino desde u hasta v
+                false -> caso contrario
+
+        Tiempo de ejecucion O(1)
+    */
+    fun existeUnCamino(u: Int, v: Int) : Boolean { 
+        if (u < 0 || u >= nVertices) {
+            throw RuntimeException("FloydWarshall.costo(): El vertice $u no pertenece al grafo")
+        }
+
+        if (v < 0 || v >= nVertices) {
+            throw RuntimeException("FloydWarshall.costo(): El vertice $v no pertenece al grafo")
+        }
+
+        return matrizPred[u][v] != null
+    }
+
+    /* 
+        Devuelve los arcos del camino de costo minimo del camino
+        desde el vertice u hasta v
+
+        {P: u y v son vertices del grafo}
+        {Q: <u, x>, <x, y>, ..., <v.pred, v>}
+
+        Input: u y v Vertices del grafo
+        Output: Los arcos del camino de costo minimo del camino desde el vertice u hasta el vertive v
+
+        Tiempo de ejecucion O(nVertices)
+    */
+    fun obtenerCaminoDeCostoMinimo(u: Int, v: Int) : Iterable<Arco> { 
+        if (u < 0 || u >= nVertices) {
+            throw RuntimeException("FloydWarshall.costo(): El vertice $u no pertenece al grafo")
+        }
+
+        if (v < 0 || v >= nVertices) {
+            throw RuntimeException("FloydWarshall.costo(): El vertice $v no pertenece al grafo")
+        }
+
+        if (!this.existeUnCamino(u, v)) {
+            throw RuntimeException("FloydWarshall.costo(): No hay camino desde $u hasta $v")
+        }
+        var currVer: Int = v
+        var camino: MutableList<Arco> = mutableListOf()
+        while (currVer != u) {
+            val pred: Int? = matrizPred[u][currVer]!!
+            camino.add(Arco(Vertice(pred!!), Vertice(currVer)))
+            currVer = pred
+        }
+
+        return camino.asIterable()
+    }
 }
